7e5135ee41f162a5419fe086e278ec46a7007760
crocs007 <s_bisht07@icloud.com>
2026-07-10 23:43:29 +0530

    corected updateProfile

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,10 @@ const app = express();
 app.use(express.json());
 
 const authRouter = require("./routes/authRoutes")
+const updateProfileRouter = require("./routes/updateProfileRoutes")
+
 app.use('/api/auth',authRouter);
+app.use('/api/profile',updateProfileRouter);
 
 const connectDB = require('./config/database');
 connectDB();
